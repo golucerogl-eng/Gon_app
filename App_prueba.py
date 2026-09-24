629e57a210eb5636a0a54648e52184748a9229a1
golucerogl-eng <golucero.gl@gmail.com>
2026-09-24 20:04:35 -0300

    Initialize Streamlit app with CSV data loading
    Add Streamlit app to display data from a CSV file

--- App_prueba.py
+++ App_prueba.py
@@ -1,1 +1,14 @@
+import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
 
+df = pd.read_csv("traj_UNI_CORR_500_01.txt", 
+                 skiprows=4, 
+                 sep="\t", 
+                 names=["ID", "frames", "X", "Y", "Z"])
+
+
+st.write("""
+# Mi primera aplicación interactiva
+## 
+""")
